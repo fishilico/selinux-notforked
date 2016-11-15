2f2f922f678c521135c1045b77692b2c23933ba1
Nicolas Iooss <nicolas.iooss@m4x.org>
2016-11-15 23:12:08 +0100

    [WIP] link the DSOs with -Bsymbolic
    TODO: add a program which overloads a global variable/function and test
    -Bsymbolic consequences
    
    This makes the libs auto-resolve internal symbols [FIXME: better
    description]
    
    Diff of objdump -T libsepol.so when hidden definition is removed
    (potential ill-exported functions?):
    + g    DF .text   LIBSEPOL_1.0 sepol_context_to_sid
    + g    DF .text   LIBSEPOL_1.0 sepol_genbools_policydb
    + g    DF .text   LIBSEPOL_1.0 sepol_msg_default_handler
    + g    DF .text   LIBSEPOL_1.0 sepol_node_sid
    + g    DF .text   LIBSEPOL_1.0 sepol_port_sid

--- libsemanage/src/dso.h
+++ libsemanage/src/dso.h
@@ -3,17 +3,8 @@
 
 #ifdef SHARED
 # define hidden __attribute__ ((visibility ("hidden")))
-# define hidden_proto(fct) __hidden_proto (fct, fct##_internal)
-# define __hidden_proto(fct, internal)	\
-     extern __typeof (fct) internal;	\
-     extern __typeof (fct) fct __asm (#internal) hidden;
-# if defined(__alpha__) || defined(__mips__)
-#  define hidden_def(fct) \
-     asm (".globl " #fct "\n" #fct " = " #fct "_internal");
-# else
-#  define hidden_def(fct) \
-     asm (".globl " #fct "\n.set " #fct ", " #fct "_internal");
-#endif
+# define hidden_proto(fct)
+# define hidden_def(fct)
 #else
 # define hidden
 # define hidden_proto(fct)
